c59b3097c3b0dc3280293cd4d3a6b053a0380638
Pallandor <roger.sejas@gmail.com>
2016-03-08 13:12:34 +1100

    Modifications to sample demo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var coolfaces = require('cool-ascii-faces');
+var url = require('url');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,12 +11,34 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+// Does ordering of routes matter? E.g. will req go through /cool
+// AND home route..
+// without the star, /cool goes to just the /cool part. so get is strict?
+// wonder with star.
+// and also with app.use and app.post . 
+
+app.get('/*', function(req, res) {
+    // res.end('the first get router, catchall?');
+    var urlObj = url.parse(req.url, true);
+    res.json(urlObj.query); // should send JSON.stringify-ed version of
+  						  // urlObj.query which should be in obj rather
+  						  // than str format. 
+    // next(); 
+});
+
+// app.get is strict with its mountPath. So if something gets
+// caught, it doesn't go to the next app.get.. 
+app.get('/cool', function(req, res) {
+	// console.log("the /cool app.get route"); 
+    res.send(coolfaces());
+    // // INVESTIGATE BELOW!!!
+    //    var x = coolfaces(); // it gives a string... 
+    //    res.write(typeof x); 
+    //    // how to test if buffer? 
+    //    res.end(); // so why does putting string in end not work??? 
+    //    // read up on node.js documentation for res.end(). 
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
